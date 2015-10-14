10227d357b1e3bf5421aca9fb67c19cc78a26545
Lauri Laaksonen <lauri@laaksonen.io>
2015-10-14 17:57:50 +0300

    Add initial content and styling

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var cssmin = require('gulp-cssmin');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
 
 var production = process.env.NODE_ENV === 'production';
@@ -11,18 +14,31 @@ var production = process.env.NODE_ENV === 'production';
 var config = {
   port: 9000,
   paths: {
-    styles: './styles/*.scss',
-    mainScss: './styles/main.scss',
+    js: './src/js/*.js',
+    styles: './src/scss/*.scss',
+    images: './src/images/**/*',
+    mainScss: './src/scss/main.scss',
+    distJs:'./dist/js',
     distCss: './dist/css',
+    distImages: './dist/images',
   }
 };
+
+/*
+ |--------------------------------------------------------------------------
+ | Minify JavaScript.
+ |--------------------------------------------------------------------------
+*/
+gulp.task('js', function() {
+  gulp.src(config.paths.js).pipe(uglify()).pipe(gulp.dest(config.paths.distJs));
+});
 
 
 /*
  |--------------------------------------------------------------------------
  | Compile scss stylesheets.
  |--------------------------------------------------------------------------
- */
+*/
 gulp.task('styles', function () {
   gulp.src(config.paths.mainScss)
     .pipe(sass().on('error', sass.logError))
@@ -42,9 +58,27 @@ gulp.task('styles-watch', ['styles'], browserSync.reload);
 
 /*
  |--------------------------------------------------------------------------
+ | Minify Images.
+ |--------------------------------------------------------------------------
+*/
+gulp.task('images', function () {
+  return gulp.src(config.paths.images)
+    .pipe(cache(imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest(config.paths.distImages));
+});
+
+
+/*
+ |--------------------------------------------------------------------------
  | Run browser-sync server and watch for changes.
  |--------------------------------------------------------------------------
- */
+*/
 gulp.task('watch', function () {
   browserSync.init({
     server: './',
@@ -59,6 +93,6 @@ gulp.task('watch', function () {
  |--------------------------------------------------------------------------
  | Run tasks
  |--------------------------------------------------------------------------
- */
+*/
 gulp.task('default', ['watch']);
-gulp.task('build', ['styles']);
+gulp.task('build', ['js', 'styles', 'images']);
